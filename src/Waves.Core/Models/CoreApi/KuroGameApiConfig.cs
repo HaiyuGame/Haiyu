de414d9fa7de131710ae6f2a72dde1319343bcba
30140 <30140@BLAME>
2026-02-09 17:55:54 +0800

    commit

--- src/Waves.Core/Models/CoreApi/KuroGameApiConfig.cs
+++ src/Waves.Core/Models/CoreApi/KuroGameApiConfig.cs
@@ -1,6 +1,9 @@
-﻿namespace Waves.Core.Models.CoreApi
+﻿using Waves.Core.Contracts;
+using Waves.Core.Models.Enums;
+
+namespace Waves.Core.Models.CoreApi
 {
-    public class KuroGameApiConfig
+    public class KuroGameApiConfig:IGameAPIConfig
     {
         public static string[] BaseAddress =
         [
@@ -152,51 +155,11 @@
                     "https://prod-alicdn-gamestarter.kurogame.com/launcher/launcher/50016_i2n5NLmdCAmOGP3J1tJOlWKNSMQuyWL7/G279/index.json",
                 Language = "zh-Hant",
             };
+
         #endregion
+
+
+        public CoreType ApiType => CoreType.Kuro;
     }
 
-    #region 旧API
-
-    //public class GameApiContextConfig
-    //{
-    //    public string Starter_Source { get; set; }
-
-    //    public string Launcher_Source { get; set; }
-
-    //    public string LauncherHeader_Source { get; set; }
-
-    //    public static GameApiContextConfig Main =>
-    //        new()
-    //        {
-    //            Starter_Source =
-    //                "https://prod-cn-alicdn-gamestarter.kurogame.com/launcher/10003_Y8xXrXk65DqFHEDgApn3cpK5lfczpFx5/G152/information/zh-Hans.json",
-    //            Launcher_Source =
-    //                "https://prod-cn-alicdn-gamestarter.kurogame.com/launcher/game/G152/10003_Y8xXrXk65DqFHEDgApn3cpK5lfczpFx5/index.json",
-    //            LauncherHeader_Source =
-    //                "https://prod-cn-alicdn-gamestarter.kurogame.com/pcstarter/prod/game/G152/10003_Y8xXrXk65DqFHEDgApn3cpK5lfczpFx5/social/zh-Hans.json",
-    //        };
-
-    //    public static GameApiContextConfig BiliBili =>
-    //        new()
-    //        {
-    //            Starter_Source =
-    //                "https://prod-cn-alicdn-gamestarter.kurogame.com/pcstarter/prod/game/G152/10004_j5GWFuUFlb8N31Wi2uS3ZAVHcb7ZGN7y/index.json",
-    //            Launcher_Source =
-    //                "https://prod-cn-alicdn-gamestarter.kurogame.com/pcstarter/prod/starter/10004_j5GWFuUFlb8N31Wi2uS3ZAVHcb7ZGN7y/G152/index.json",
-    //            LauncherHeader_Source =
-    //                "https://prod-cn-alicdn-gamestarter.kurogame.com/pcstarter/prod/game/G152/10004_j5GWFuUFlb8N31Wi2uS3ZAVHcb7ZGN7y/social/zh-Hans.json",
-    //        };
-
-    //    public static GameApiContextConfig Global =>
-    //        new()
-    //        {
-    //            Starter_Source =
-    //                "https://prod-alicdn-gamestarter.kurogame.com/pcstarter/prod/game/G153/50004_obOHXFrFanqsaIEOmuKroCcbZkQRBC7c/index.json",
-    //            Launcher_Source =
-    //                "https://prod-alicdn-gamestarter.kurogame.com/pcstarter/prod/starter/50004_obOHXFrFanqsaIEOmuKroCcbZkQRBC7c/G153/index.json",
-    //            LauncherHeader_Source =
-    //                "https://prod-alicdn-gamestarter.kurogame.com/pcstarter/prod/game/G153/50004_obOHXFrFanqsaIEOmuKroCcbZkQRBC7c/social/zh-Hant.json",
-    //        };
-    //}
-    #endregion
 }
