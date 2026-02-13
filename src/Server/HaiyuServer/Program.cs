2d8b13d0cbc9a151ef40e15af36a96820a6a9ba9
30140 <30140@BLAME>
2026-02-13 17:38:55 +0800

    commit

--- src/Server/HaiyuServer/Program.cs
+++ src/Server/HaiyuServer/Program.cs
@@ -7,7 +7,6 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllers();
         var app = builder.Build();
-        app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
         app.Run();
