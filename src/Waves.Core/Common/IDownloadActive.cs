84c5b3fe87a35f37a408b874d1042eb6a872cf17
30140 <30140@BLAME>
2026-03-05 16:37:54 +0800

    commit

---
+++ src/Waves.Core/Common/IDownloadActive.cs
@@ -0,0 +1,11 @@
+﻿namespace Waves.Core.Common;
+
+/// <summary>
+/// 下载执行时的事件通知
+/// </summary>
+public interface IDownloadActive
+{
+    public void Init();
+
+    public Task ActiveAsync(Task eventMethod);
+}
