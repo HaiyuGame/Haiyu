1eade8a24740fa71143deea1ef4be68392493afa
Blame <3014085426@qq.com>
2026-03-11 22:05:58 +0800

    打包测试

--- src/WutheringWavesTool/App.xaml.cs
+++ src/WutheringWavesTool/App.xaml.cs
@@ -18,7 +18,7 @@ public partial class App : ClientApplication
     private const int PROCESS_PER_MONITOR_DPI_AWARE = 2;
     private AppInstance mainInstance;
 
-    public static string AppVersion => "1.2.19";
+    public static string AppVersion => "1.2.20";
 
     public AppSettings AppSettings { get; private set; }
 
